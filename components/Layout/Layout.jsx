57e574e1894696fc3c45301bd552234eadd92755
dathoang <dathoang31082001@gmail.com>
2022-09-17 11:05:30 +0700

    update .eslintrc.json

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 
-import NavBar from '../Navbar/NavBar';
+import NavBar from '../NavBar/NavBar';
 import Footer from '../Footer/Footer';
 
 const Layout = ({ children }) => {
